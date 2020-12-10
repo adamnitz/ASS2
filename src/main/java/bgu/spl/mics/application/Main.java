4aba0f7a28415ccd12386d01ffc0784f2cb05d22
Meitar <meitargo@post.bgu.ac.il>
2020-12-10 17:36:11 +0200

    changes microServices

--- src/main/java/bgu/spl/mics/application/Main.java
+++ src/main/java/bgu/spl/mics/application/Main.java
@@ -1,14 +1,13 @@
 package bgu.spl.mics.application;
 
-import bgu.spl.mics.application.passiveObjects.Attack;
-import bgu.spl.mics.application.passiveObjects.Ewoks;
-import bgu.spl.mics.application.passiveObjects.Input;
-import bgu.spl.mics.application.passiveObjects.JsonInputReader;
+import bgu.spl.mics.MessageBusImpl;
+import bgu.spl.mics.application.passiveObjects.*;
 
 import bgu.spl.mics.application.services.LeiaMicroservice;
 import com.google.gson.Gson;
 
 
+import java.awt.*;
 import java.awt.font.TextHitInfo;
 import java.io.IOException;
 
@@ -17,20 +16,23 @@ import java.io.IOException;
  * In the end, you should output a JSON.
  */
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		//reading Input
-	try{
+
 		String input = args[1];
 		String output = args[2];
 
 		Input json = JsonInputReader.getInputFromJson(input);
 		System.out.println(json);
 
-		//Ewoks ewoks = new Ewoks(json.getEwoks());
+		MessageBusImpl messageBus = MessageBusImpl.getInstance();
+		Ewoks ewoks = Ewoks.getInstance();
+		ewoks.setEwoks(json.getEwoks());
+		Diary diary = Diary.getInstance();
 
-	} catch (IOException e) {
-		e.printStackTrace();
-	}
+		LeiaMicroservice leia = new LeiaMicroservice(input)
+
+
 
 		//	Diary diary = new Diary();
 		//Thread Leia = new Thread(LeiaMicroservice);
